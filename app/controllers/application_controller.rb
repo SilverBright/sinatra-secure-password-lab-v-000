fe5b0296bc550dfb1c5ddd1d56c1c06d2596b7e4
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-23 01:24:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,16 +53,14 @@ class ApplicationController < Sinatra::Base
     erb :withdraw
   end
 
-
   get "/login" do
     erb :login
   end
 
   post "/login" do
-
     if params[:username].empty? || params[:password].empty?
       redirect to '/failure'
-    else
+      else
       user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
